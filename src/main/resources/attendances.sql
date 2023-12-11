e19a2f719495f385e1545130f3ccc015a84c48b1
Masaki0815 <150638038+Masaki0815@users.noreply.github.com>
2023-12-11 10:43:27 +0900

    Add files via upload

---
+++ src/main/resources/attendances.sql
@@ -0,0 +1,128 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 12.16 (Ubuntu 12.16-0ubuntu0.20.04.1)
+-- Dumped by pg_dump version 12.16 (Ubuntu 12.16-0ubuntu0.20.04.1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: data; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.data (
+    begin_time character varying(100),
+    end_time character varying(100),
+    name character varying(100),
+    id integer NOT NULL,
+    work_space integer
+);
+
+
+ALTER TABLE public.data OWNER TO postgres;
+
+--
+-- Name: attendances_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.attendances_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.attendances_id_seq OWNER TO postgres;
+
+--
+-- Name: attendances_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.attendances_id_seq OWNED BY public.data.id;
+
+
+--
+-- Name: attendances_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.attendances_seq
+    START WITH 1
+    INCREMENT BY 50
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.attendances_seq OWNER TO postgres;
+
+--
+-- Name: seq_id; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.seq_id
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.seq_id OWNER TO postgres;
+
+--
+-- Name: data id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.data ALTER COLUMN id SET DEFAULT nextval('public.attendances_id_seq'::regclass);
+
+
+--
+-- Data for Name: data; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.data (begin_time, end_time, name, id, work_space) FROM stdin;
+2023年12月10日 21時15分日曜日	2023年12月10日 21時15分日曜日	aaa	2	1
+\.
+
+
+--
+-- Name: attendances_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.attendances_id_seq', 2, true);
+
+
+--
+-- Name: attendances_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.attendances_seq', 5051, true);
+
+
+--
+-- Name: seq_id; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.seq_id', 5, true);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
